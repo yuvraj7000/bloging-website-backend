44d6753acf4165f7f7d929e3a1136d95259aae4c
Yuvraj Yadav <yuvraj7000raju@gmail.com>
2024-10-02 00:23:39 +0530

    delete blog, save blog, unsave blog, show user blogs, get saved blogs,  <- all of this functionality is done

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -36,6 +36,30 @@ const createBlog = async (req, res) => {
 
 } 
 
+const deleteBlog = async (req, res) => {
+    const{blog_id} = req.body;
+    if(!blog_id){
+        return res.status(400).json({message: 'blog_id is required'});
+    }
+    if(!req.user){
+        return res.status(400).json({message: 'login is required'});
+    }
+    const blog = await Blog.findById(blog_id);
+    if(!blog){
+        return res.status(400).json({message: 'Blog not found'});
+    }
+    console.log(blog.created_by,"----", req.user._id);
+    if(blog.created_by.toString() != req.user._id.toString()){
+        return res.status(400).json({message: 'You are not authorized to delete this blog'});
+    }
+    await Blog.findByIdAndDelete(blog_id);
+    
+    const req_user = await User.findById(req.user._id).populate('blogs');
+    return res.status(200).json({message: 'successfully deleted blog', req_user});
+}
+
+
+
 const star_blog = async (req, res) => {
     const {blog_id} = req.body;
     if(!blog_id){
@@ -57,6 +81,47 @@ const star_blog = async (req, res) => {
 
 }
 
+const unstar_blog = async (req, res) => {
+    const {blog_id} = req.body;
+    if(!blog_id){
+        return res.status(400).json({message: 'blog_id is required'});
+    }
+    const user = await User.findById(req.user._id);
+    if(!user){
+        return res.status(400).json({message: 'User not found. || login is required'});
+    }
+
+    const blog = await Blog.findById(blog_id);
+    if(!blog){
+        return res.status(400).json({message: 'Blog not found'});
+    }
+    user.stars = user.stars.filter(star => star != blog_id);
+    await user.save();
+
+    res.status(200).json({message: 'Blog unsaved successfully', user});
+}
+
+const get_star_blogs = async (req, res) => {
+    const user = await User.findById(req.user._id).select("username user_img stars").populate('stars');
+    if(!user){
+        return res.status(400).json({message: 'User not found. || login is required'});
+    }
+    res.status(200).json({message: 'getting star blogs successful', user});
+
+}
+
+const user_blogs = async (req, res) => {
+    const {username} = req.body;
+    console.log("username", username);
+    const blogs = await User.find({username,}).populate('blogs').select('blogs username user_img');
+    res.status(200).json({message:"getting blogs successful", blogs});
+}
 
 
-export { createBlog, star_blog };
+
+export { createBlog,
+        deleteBlog,
+     star_blog, unstar_blog,
+     user_blogs,
+     get_star_blogs,
+    };
--- src/routers/blog.router.js
+++ src/routers/blog.router.js
@@ -1,11 +1,15 @@
-import { createBlog, star_blog } from "../controllers/blog.controller.js";
+import { createBlog,deleteBlog, star_blog, unstar_blog, user_blogs, get_star_blogs } from "../controllers/blog.controller.js";
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 const blog_router = Router();
 
 blog_router.route("/create_blog").post(verifyJWT,upload.single('blog_img'),createBlog);
+blog_router.route("/delete_blog").post(verifyJWT,deleteBlog);
 blog_router.route("/star_blog").post(verifyJWT, star_blog);
+blog_router.route("/unstar_blog").post(verifyJWT, unstar_blog);
+blog_router.route("/get_star_blogs").get(verifyJWT, get_star_blogs);
+blog_router.route("/user_blogs").post( user_blogs);
 
 
 export default blog_router;
